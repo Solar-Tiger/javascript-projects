8505745f43c3cd99820480ad353cd85778c42777
David Singleton <davidjsingleton18@gmail.com>
2023-05-29 10:36:04 -0700

    Add hangman logic for safe keeping

--- rock-paper-scissors/rock-paper-scissors.js
+++ rock-paper-scissors/rock-paper-scissors.js
@@ -107,3 +107,44 @@ resetButton.addEventListener('click', () => {
 
   playerChoices.addEventListener('click', gameTime);
 });
+
+// let string = 'whoever';
+// let arr = ['x', 'x', 'x', 'x', 'x', 'x', 'x'];
+// let newArr = string.split('');
+// let usedArr = [];
+// let hang = 0;
+
+// function compareLetter() {
+//   if (hang !== 8) {
+//     let letter = prompt('Enter letter');
+//     let miss = 0;
+
+//     for (let i = 0; i < usedArr.length; i++) {
+//       if (usedArr[i] === letter) {
+//         console.log('Already used, try again');
+//         return;
+//       }
+//     }
+
+//     for (let i = 0; i < arr.length; i++) {
+//       if (newArr[i] === letter) {
+//         arr.splice(i, 1, letter);
+//       } else {
+//         miss++;
+//       }
+//     }
+
+//     if (miss === newArr.length) {
+//       hang++;
+//     }
+
+//     usedArr.push(letter);
+
+//     console.log(usedArr);
+//     console.log(arr);
+//     console.log(hang);
+//   } else {
+//     console.log('Game over');
+//     return 'Game over';
+//   }
+// }
